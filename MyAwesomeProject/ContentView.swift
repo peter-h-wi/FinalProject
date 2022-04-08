497dea4d08b6c6c68d788ee5fc78a25a15565379
Peter Wi <peter.h.wi96@gmail.com>
2022-04-07 23:40:20 -0400

    add files and gitignore

---
+++ MyAwesomeProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAwesomeProject
+//
+//  Created by peter wi on 4/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAwesomeProject/MyAwesomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAwesomeProjectApp.swift
+//  MyAwesomeProject
+//
+//  Created by peter wi on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAwesomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
